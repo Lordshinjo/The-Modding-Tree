e56a76a438b7c943c4b7e9434a3e11bddb95a893
Rémy Pecqueur <1932432+Lordshinjo@users.noreply.github.com>
2022-02-16 15:56:01 +0900

    ✨ Allow loading remote mods from loader.js
    We remove every other script from index.html, and introduce 2 loading
    modes:
    - Light loading mode will get all technical files from us, and load all
    mod files from the mod repository
    - Full loading mode will try to load all files (except loader.js) from
    the mod repository
    Mods are loaded from query parameters:
    - user for the GitHub user
    - repo for the name of the repo (defaults to 'The-Modding-Tree')
    - branch for the branch to fetch (defaults to 'master')
    - mode=full to trigger full loading mode

--- js/technical/loader.js
+++ js/technical/loader.js
@@ -1,9 +1,101 @@
-// Load files
-
-for (file in modInfo.modFiles) {
-    let script = document.createElement("script");
-    script.setAttribute("src", "js/" + modInfo.modFiles[file]);
-    script.setAttribute("async", "false");
-    document.head.insertBefore(script, document.getElementById("temp"));
+function appendScript(baseUrl, path) {
+    return new Promise((resolve, reject) => {
+        const url = `${baseUrl}${path}`;
+        const script = document.createElement('script');
+        script.src = url;
+        script.async = false;
+        script.onload = resolve;
+        script.onerror = e => reject(Error(`${url} failed to load`));
+        document.head.appendChild(script);
+    });
 }
 
+function appendAllScripts(baseUrl, paths) {
+    const promises = [];
+    for (let path of paths) {
+        promises.push(appendScript(baseUrl, path));
+    }
+    return Promise.all(promises);
+}
+
+async function loadFullMod(baseUrl) {
+    const indexResponse = await fetch(`${baseUrl}index.html`);
+    const indexText = await indexResponse.text();
+    const allMatches = [...indexText.matchAll(/<script\s+src="(js\/[^"]+)">/g)];
+
+    const beforeFiles = [];
+    const afterFiles = [];
+    let isBefore = true;
+    for (match of allMatches) {
+        if (match[1] === 'js/technical/loader.js') {
+            isBefore = false;  // And do not load that file or it will try to load wrong files
+        } else if (isBefore) {
+            beforeFiles.push(match[1]);
+        } else {
+            afterFiles.push(match[1]);
+        }
+    }
+
+    await appendAllScripts(baseUrl, beforeFiles);
+    await appendAllScripts(`${baseUrl}js/`, modInfo.modFiles);
+    await appendAllScripts(baseUrl, afterFiles);
+}
+
+async function loadLightMod(baseUrl) {
+    const beforeFiles = [
+        'technical/break_eternity.js',
+        'technical/layerSupport.js',
+    ];
+    const afterFiles = [
+        'technical/temp.js',
+        'technical/displays.js',
+        'game.js',
+        'utils.js',
+        'utils/easyAccess.js',
+        'technical/systemComponents.js',
+        'components.js',
+        'technical/canvas.js',
+        'technical/particleSystem.js',
+        'utils/NumberFormating.js',
+        'utils/options.js',
+        'utils/save.js',
+        'utils/themes.js',
+    ];
+    await appendAllScripts('js/', beforeFiles);
+    await appendScript(baseUrl, 'js/mod.js');
+    await appendAllScripts(`${baseUrl}js/`, modInfo.modFiles);
+    await appendAllScripts('js/', afterFiles);
+}
+
+function loadMod() {
+    const params = new URLSearchParams(window.location.search);
+    const user = params.get('user');
+
+    if (!user) {
+        throw new Error('Github user not specified');
+    }
+
+    const repo = params.get('repo') || 'The-Modding-Tree';
+    const branch = params.get('branch') || 'master';
+    return fetch(`https://api.github.com/repos/${user}/${repo}/branches/${branch}`)
+        .then(async (response) => {
+            const data = await response.json()
+            const commit = data['commit']['sha'];
+            const baseUrl = `//cdn.jsdelivr.net/gh/${user}/${repo}@${commit}/`;
+
+            if (params.get('mode') === 'full') {
+                await loadFullMod(baseUrl);
+            } else {
+                await loadLightMod(baseUrl);
+            }
+
+            load();
+            document.getElementById('loadingSection').style = 'display: none';
+            document.getElementById('app').style = null;
+            document.body.onmousemove = event => updateMouse(event);
+        })
+        .catch(err => console.error(err));
+    ;
+}
+
+loadMod();
